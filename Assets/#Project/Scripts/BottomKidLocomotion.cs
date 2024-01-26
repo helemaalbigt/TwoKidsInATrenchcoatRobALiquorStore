5015826a94f38374ef24c6b299374881ea718b70
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2024-01-26 23:20:10 +0100

    basic movement

--- Assets/#Project/Scripts/BottomKidLocomotion.cs
+++ Assets/#Project/Scripts/BottomKidLocomotion.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
 
 public class BottomKidLocomotion : MonoBehaviour {
-    public Rigidbody Rigidbody;
+    public Rigidbody rigidBody;
+    public float speed;
+    public float rotationSpeed;
     
-    void Update()
-    {
+    void FixedUpdate() {
+        Vector3 movement = Vector3.zero;
+        Vector3 angularVel = Vector3.zero;
         
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            movement += transform.forward * speed;
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            movement -= transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            angularVel += new Vector3(0, -rotationSpeed, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            angularVel += new Vector3(0, rotationSpeed, 0);
+        }
+
+        rigidBody.velocity = movement;
+        rigidBody.angularVelocity = angularVel;
     }
 }
