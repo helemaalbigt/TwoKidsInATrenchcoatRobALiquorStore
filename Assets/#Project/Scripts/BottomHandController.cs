f67f809cc7183f50b603bdff190871d4fc4fa798
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2024-01-27 13:52:50 +0100

    wip pcoket positioning

--- Assets/#Project/Scripts/BottomHandController.cs
+++ Assets/#Project/Scripts/BottomHandController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class BottomHandController : MonoBehaviour
 {
     public LayerMask hittableHandLayers;
+    public LayerMask snappableLayers;
+    
     public Camera camera;
     public Transform hand;
     public MouseGrabber grabber;
@@ -13,9 +15,29 @@ public class BottomHandController : MonoBehaviour
     public float offsetGrab = 0.04f;
     
     private Vector3 _targetHandPos;
+    private bool _grabbedThisFrame;
+    private bool _grabbedLastFrame;
+
+    private Transform _grabbedObject;
+    private Transform _snapAnchor;
     
     private void Update() {
+        _grabbedThisFrame = grabber.grabbedObject;
         PositionHand();
+        
+        if (_grabbedThisFrame && !_grabbedLastFrame) {
+            //_grabbedObject = grabber.
+        }
+
+        if (_grabbedThisFrame) {
+            CheckForSnapAnchor();
+        }
+
+        if (!_grabbedThisFrame && _grabbedLastFrame) {
+            CheckToPlaceObject();
+        }
+        
+        _grabbedLastFrame = _grabbedThisFrame;
     }
 
     private void PositionHand() {
@@ -26,5 +48,18 @@ public class BottomHandController : MonoBehaviour
         }
 
         hand.position = Vector3.Slerp(hand.position, _targetHandPos, Time.unscaledDeltaTime * 8f);
+    }
+
+    private void CheckForSnapAnchor() {
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 2f, snappableLayers)) {
+            _snapAnchor = hit.transform;
+        } else {
+            _snapAnchor = null;
+        }
+    }
+
+    private void CheckToPlaceObject() {
+        
     }
 }
