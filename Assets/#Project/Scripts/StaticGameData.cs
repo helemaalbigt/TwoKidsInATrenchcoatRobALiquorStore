d79e9f3f1704591dd96b6736351c60037bc2aa9b
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2024-01-27 23:35:03 +0100

    staticgamedata

---
+++ Assets/#Project/Scripts/StaticGameData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticGameData {
+    public static bool gotCaught; 
+    public static List<Bottle> stolenBottles;
+}
